e5ba0e25e87e21e11b88ea0493e18d88c2554f3b
Honey <Him17anshu@gmail.com>
2022-09-27 00:14:52 +0530

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/18/204231f1623d001d1871a4aa34496c44
@@ -0,0 +1,8 @@
+package com.in28minutes;
+
+public class App {
+	public String returnSomething() {
+		return "DUMMY";
+	}
+
+}
---
+++ first-maven-project/src/main/java/com/in28minutes/App.java
@@ -0,0 +1,13 @@
+package com.in28minutes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class App {
+	public String returnSomething() {
+		return "DUMMY";
+	}
+
+}
